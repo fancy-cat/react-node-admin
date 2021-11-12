d70bcf108d3396c6c8dba0b46ece8d7106facfc7
wangyafen <wangyafen@suirem.com>
2021-11-12 15:13:15 +0800

    删除多余内容

--- backend/app.js
+++ backend/app.js
@@ -31,43 +31,20 @@ app.all("*", (req, res, next) => {
   //设置允许跨域的域名，*代表允许任意域名跨域
   res.header("Access-Control-Allow-Origin","http://localhost:3001");
   //允许的header类型
-  // res.header("Access-Control-Allow-Headers","content-type");
+  res.header("Access-Control-Allow-Headers","content-type");
   res.header("Content-Type", "application/json")
   next()
 })
-// app.get('/', (req, res) => {
-//   console.log(req)
-//   res.send('Hello world')
-// })
 
-// 医生列表
-app.get('/doctor/getDoctorPage', (req, res) => {
-  // -------- -------- -------- -------- 读取本地json文件
-  // const list = fs.readFileSync(path.resolve(__dirname, './data/doctor.json'), 'utf8')
-  // const resData = formDataRes(0, {
-  //   list: JSON.parse(list)
-  // })
-  // 返回的文件 must be one of type string or Buffer
-  // res.end(resData)
-  // --------- -------- -------- -------- mongoDB   
-  // console.log(doctor)
+// 医生列表查询
+app.get('/doctor/getDoctorPage', (req, res) => { 
   Doctor.find((err, docs) => {
     const resData = formDataRes(0, {
       list: docs
     })
+    // 返回的文件 must be one of type string or Buffer
     res.end(resData)
   })
-})
-
-// 医生详情
-app.get('/doctor/detail', (req, res) => {
-  let queryData = req.query
-  const list = fs.readFileSync(path.resolve(__dirname, './data/doctor.json'), 'utf8')
-  const target = JSON.parse(list).find(v => v.id == queryData.id)
-  const resData = formDataRes(0, {
-    info: target
-  })
-  res.end(resData)
 })
 
 app.listen(port, () => {
